4d86de7103ed5de9af0e42c6f6e660c81a35e6ef
Ebbi Gebbi <smolen89@gmail.com>
2019-07-17 20:10:47 +0200

    Create NamespaceHolder.cs

---
+++ Assets/Scripts/Utils/NamespaceHolder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class NamespaceHolder : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+//}
+
+namespace DRL { }
+namespace DRL.Engine { }
+namespace DRL.Engine.Systems { }
+namespace DRL.Engine.Utils { }
+
+
+namespace DRL.Engine.MessageSystem { }
+namespace DRL.Engine.GameCommandSysyem { }
+namespace DRL.Engine.GrafringSystem { }
+namespace DRL.Engine.ItemManagmentSystem { }
+namespace DRL.Engine.BloodSystem { }
+
+namespace DRL.GameEngine.ECS.Entities { }
+namespace DRL.GameEngine.ECS.Pathfinding { }
+namespace DRL.Utils { }
+namespace DRL.GameEngine.ECS.Systems.Effects { }
