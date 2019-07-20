bef2028f8f626d4e4c4d4c568c817d14009b7020
Ebbi Gebbi <smolen89@gmail.com>
2019-07-20 03:05:56 +0200

    Create GameEventSystem.cs

---
+++ Assets/Scripts/GameEngine/Systems/GameEventSystem/GameEventSystem.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) 2019 EG Studio, LLC. All Rights Reserved.
+// Create by Ebbi Gebbi
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RDE.Engine.Systems
+{
+	public abstract class GameEvent<T> where T: GameEvent<T>
+	{
+		private bool hasFired;
+		private static event EventListener listeners;
+
+		public string Description;
+		public delegate void EventListener( T info );
+		
+		public static void RegisterListener(EventListener listener )
+		{
+			listeners += listener;
+		}
+
+		public static void UnregisterListener(EventListener listener )
+		{
+			listeners -= listener;
+		}
+
+		public void FireEvent()
+		{
+			if( hasFired )
+			{
+				Debugger.Log( "GameEvent", $"This event has already fired, to prevent infinite loops you can't refire an event." );
+				return;
+			}
+
+			hasFired = true;
+
+			listeners?.Invoke( this as T );
+		}
+	} 
+
+	public class DebugEvent : GameEvent<DebugEvent>
+	{
+		public int VerbosityLevel;
+	}
+
+	public class UnitDeathEvent : GameEvent<UnitDeathEvent>
+	{
+		public GameObject UnitGO;
+		// Info about cause of death, our killer, etc..
+		// Could be a struct, readonly, etc..
+	}
+
+}
